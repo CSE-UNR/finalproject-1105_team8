16efb8c4b615aee62b4963404dd8c2a2f5734ba4
Andres Cuevas <andresc@nevada.unr.edu>
2024-04-29 23:32:18 +0000

    made 1st menu

--- andrescedit.c
+++ andrescedit.c
@@ -1,1 +1,38 @@
-//Andres Cuevas Final Project 
+// Andres Cuevas III
+// Erin Keith
+// CS 135
+// May 7, 2024
+// Final Project
+
+#include<stdio.h>
+void menu
+void editmenu
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+int menu(){
+int decision;
+	printf("What would you like to do?\n");
+	printf("1. Load a New Image \n");
+	printf("2. Display Current Image \n");
+	printf("3. Edit the Current Image \n");
+	printf("4. Exit the Program");
+	scanf(" %d", &decision);
+return decision;
+}
+
+
+
