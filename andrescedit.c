51e70ea638be716e7e7500cb819fdd8453fc35f3
Andres Cuevas <andresc@nevada.unr.edu>
2024-04-29 23:43:37 +0000

    made 1st menu

--- andrescedit.c
+++ andrescedit.c
@@ -31,8 +31,42 @@ int decision;
 	printf("3. Edit the Current Image \n");
 	printf("4. Exit the Program");
 	scanf(" %d", &decision);
-return decision;
+	
+	return decision;
 }
+int editmenu(){
+	int decision;
+	printf("What Edit Would You Like to Make? \n");
+	printf("1. Crop Image \n");
+	printf("2. Dim Image \n");
+	printf("3. Brighten Image \n");
+//	printf("4. Rotate Image");
+	scanf(" %d", &decision);
+
+	return decision;
+}
+int savemenu(){
+	int decision;
+	printf("Would You Like to Save? \n");
+	printf("1. Yes \n");
+	printf("2. No \n");
+	scanf(" %d", &decision);
+	
+	return decision;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
