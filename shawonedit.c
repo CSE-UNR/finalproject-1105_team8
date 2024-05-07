22d04a04548030d54e9c2e1e22f6919691581bb8
Shawon Razzak <shawonrazzak@gmail.com>
2024-05-07 21:28:28 +0000

    final

--- shawonedit.c
+++ shawonedit.c
@@ -1,4 +1,36 @@
-#include <stdio.h> 
 
 
+#include <stdio.h>
+#define FILE_NAME_LIMIT 50
+#define HEIGHTMAX 1000
+#define WIDTHMAX 1000
 
+void RetrieveNewImageData(char *filename, int *width, int *height, int imagearray[][HEIGHTMAX]) {
+    int i, columns = 0, rows = 1, totalpixels = 0;
+    FILE *filepointer = fopen(filename, "r");
+    if (filepointer == NULL) {
+        printf("Could not find an image with that filename.\n");
+        return;
+    }
+
+    while (fscanf(filepointer, "%d", &i) != EOF) {
+        totalpixels++;
+        if (i == '\n') {
+            rows++;
+        }
+    }
+    columns = totalpixels / rows;
+    fclose(filepointer);
+
+    filepointer = fopen(filename, "r");
+    *height = rows;
+    *width = columns;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            fscanf(filepointer, "%d", &imagearray[i][j]);
+        }
+    }
+
+    fclose(filepointer);
+}
